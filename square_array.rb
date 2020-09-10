5372cc2b845e0bb000ee4614c5deb307b984541a
Brock Byrd <brockbyrdd@gmail.com>
2020-09-10 16:28:03 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each { |num| new_array << num ** 2 }
+  new_array
+end
+
+numbers = [1, 2, 3]
+
+def square_array(numbers)
+  
 end
